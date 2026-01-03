58213ee99c51a41ca1e561e63d3123698c71bbc4
elodie.ughetto <elodie.ughetto@universite-paris-saclay.fr>
2026-01-04 00:15:03 +0100

    modifs des heuristiques (les 3 de bases définis dans le rapport + empêcher licorne d'aller au centre + garde rapprochée + aggressivité pour prendre licrone adverse)

--- src/main/java/games/escampe/EscampeHeuristics.java
+++ src/main/java/games/escampe/EscampeHeuristics.java
@@ -1,94 +1,108 @@
 package games.escampe;
 
-import java.util.HashSet;
 import iialib.games.algs.IHeuristic;
 
 public class EscampeHeuristics {
 
-    private static final int BLOCK_WEIGHT = 100; // poids énorme si adversaire bloqué
+    public static final int VICTORY = 1000000;
+    public static final int DEFEAT  = -1000000;
 
-    public static IHeuristic<EscampeBoard,EscampeRole>  hWhite = (board,role) -> {
+    public static IHeuristic<EscampeBoard, EscampeRole> hWhite =
+            (board, role) -> evaluate(board, EscampeRole.WHITE);
 
-		//Condition 1 : Mobilité 
-		int nbPossibleMovesWhite = board.possibleMoves(EscampeRole.WHITE).size();
-		int nbPossibleMovesBlack = board.possibleMoves(EscampeRole.BLACK).size();
-		int possibleMovesScore = nbPossibleMovesWhite - nbPossibleMovesBlack; 
-		if(nbPossibleMovesBlack==0) possibleMovesScore =+ BLOCK_WEIGHT; // accorder un poids énorme aux situations où la mobilité de l'adversaire tombe à 0
+    public static IHeuristic<EscampeBoard, EscampeRole> hBlack =
+            (board, role) -> evaluate(board, EscampeRole.BLACK);
 
-		//Condition 2 : Sécurité de la Licorne 
-		int dangerScore = 0;
-		long blackPaladins = board.getBlackPaladins();
-        long whiteUnicorn = board.getWhiteUnicorn();
-        for(int i=0; i<36; i++) {
-            if((whiteUnicorn & (1L<<i))!=0) { // Licorne blanche
-                for(int j=0; j<36; j++) {
-                    if((blackPaladins & (1L<<j))!=0) {
-                        // Manhattan distance pour menace
-                        int dx = Math.abs((i%6) - (j%6));
-                        int dy = Math.abs((i/6) - (j/6));
-                        int dist = dx + dy;
-                        if(dist==1) dangerScore += 1; // menace immédiate
-                        else dangerScore += 1.0/dist; // plus loin = moins dangereux
+    private static int evaluate(EscampeBoard board, EscampeRole role) {
+        boolean isWhite = (role == EscampeRole.WHITE);
+        EscampeRole oppRole = isWhite ? EscampeRole.BLACK : EscampeRole.WHITE;
+
+        long myPaladins  = isWhite ? board.getWhitePaladins() : board.getBlackPaladins();
+        long myUni       = isWhite ? board.getWhiteUnicorn() : board.getBlackUnicorn();
+        long oppPaladins = isWhite ? board.getBlackPaladins() : board.getWhitePaladins();
+        long oppUni      = isWhite ? board.getBlackUnicorn() : board.getWhiteUnicorn();
+        long allPieces   = myPaladins | myUni | oppPaladins | oppUni;
+
+        // --- 0) Sécurité immédiate ---
+        if (myUni == 0)  return DEFEAT;
+        if (oppUni == 0) return VICTORY;
+
+        int score = 0;
+        int myUniIdx = Long.numberOfTrailingZeros(myUni);
+        int oppUniIdx = Long.numberOfTrailingZeros(oppUni);
+        int myUniX = myUniIdx % 6;
+        int myUniY = myUniIdx / 6;
+
+        // --- 1) MOBILITÉ & RESTRICTION ---
+        int myMoves  = board.possibleMoves(role).size();
+        int oppMoves = board.possibleMoves(oppRole).size();
+        score += 15 * myMoves - 20 * oppMoves;
+        if (oppMoves == 0) score += 2000;
+
+        // --- 2) DIVERSITÉ DES LISERÉS ---
+        int lisereMask = 0;
+        long temp = myPaladins;
+        while (temp != 0) {
+            int idx = Long.numberOfTrailingZeros(temp);
+            int lisere = EscampeBoard.getLisereType(idx);
+            if (lisere > 0) lisereMask |= (1 << lisere);
+            temp &= (temp - 1);
+        }
+        score += 150 * Integer.bitCount(lisereMask);
+
+        // --- 3) SÉCURITÉ DE MA LICORNE (Défense) ---
+        // a) Danger direct : Paladins adverses pouvant m'atteindre
+        score -= calculateThreatWithDistance(oppPaladins, myUniIdx, allPieces, true);
+
+        // b) Prudence Géographique : Empêcher la licorne d'aller au centre
+        if (isWhite) {
+            if (myUniY > 1) score -= (myUniY * 400); // Malus si elle descend trop
+        } else {
+            if (myUniY < 4) score -= ((5 - myUniY) * 400); // Malus si elle monte trop
+        }
+
+        // c) Garde rapprochée : Bonus pour les paladins adjacents (bloqueurs)
+        temp = myPaladins;
+        while (temp != 0) {
+            int pIdx = Long.numberOfTrailingZeros(temp);
+            int dist = Math.abs(myUniX - (pIdx % 6)) + Math.abs(myUniY - (pIdx / 6));
+            if (dist == 1) score += 250; // Bonus pour chaque garde du corps direct
+            temp &= (temp - 1);
+        }
+
+        // --- 4) AGRESSIVITÉ (Attaque) ---
+        // Menace sur la licorne adverse
+        score += calculateThreatWithDistance(myPaladins, oppUniIdx, allPieces, false);
+
+        return score;
+    }
+
+    private static int calculateThreatWithDistance(long attackers, int targetIdx, long allPieces, boolean isDefensive) {
+        int threatScore = 0;
+        long temp = attackers;
+
+        while (temp != 0) {
+            int attackerIdx = Long.numberOfTrailingZeros(temp);
+            long[] paths = EscampeBoard.PATH_CACHE[attackerIdx][targetIdx];
+
+            if (paths != null) {
+                int minSteps = Integer.MAX_VALUE;
+                for (long pathMask : paths) {
+                    if ((pathMask & allPieces) == 0) {
+                        int steps = Long.bitCount(pathMask); 
+                        if (steps < minSteps) minSteps = steps;
                     }
                 }
-            }
-        }
-		
-		//Condition 3 : Diversité des contraintes 
-		int diverseLisereScore = 0;
-		long myPieces = board.getWhitePaladins() | board.getWhiteUnicorn();
-        HashSet<Integer> lisereSet = new HashSet<>();
-        for(int i=0; i<36; i++) {
-            if((myPieces & (1L<<i))!=0) {
-                lisereSet.add(board.getLisereType(i));
-            }
-        }
-        diverseLisereScore = lisereSet.size();
-
-		return possibleMovesScore + dangerScore + diverseLisereScore ; // à voir pour ajuster les poids de chaque condition 
-
-	};
-
-
-	public static IHeuristic<EscampeBoard,EscampeRole> hBlack = (board,role) -> {
-        
-		//Condition 1 : Mobilité 
-		int nbPossibleMovesWhite = board.possibleMoves(EscampeRole.WHITE).size();
-		int nbPossibleMovesBlack = board.possibleMoves(EscampeRole.BLACK).size();
-		int possibleMovesScore = nbPossibleMovesBlack - nbPossibleMovesWhite; 
-		if(nbPossibleMovesWhite==0) possibleMovesScore =+ 100; // accorder un poids énorme aux situations où la mobilité de l'adversaire tombe à 0
-
-		//Condition 2 : Sécurité de la Licorne 
-		int dangerScore = 0;
-		long whitePaladins = board.getWhitePaladins();
-        long blackUnicorn = board.getBlackUnicorn();
-        for(int i=0; i<36; i++) {
-            if((blackUnicorn & (1L<<i))!=0) { // Licorne noire
-                for(int j=0; j<36; j++) {
-                    if((whitePaladins & (1L<<j))!=0) {
-                        // Manhattan distance pour menace
-                        int dx = Math.abs((i%6) - (j%6));
-                        int dy = Math.abs((i/6) - (j/6));
-                        int dist = dx + dy;
-                        if(dist==1) dangerScore += 1; // menace immédiate
-                        else dangerScore += 1.0/dist; // plus loin = moins dangereux
-                    }
+                if (minSteps != Integer.MAX_VALUE) {
+                    // Score très élevé (1000) pour une capture en 1 coup (minSteps 0, 1 ou 2 selon liseré)
+                    // On utilise une valeur plancher pour que la capture soit toujours prioritaire
+                    int val = 1000 / (minSteps + 1);
+                    threatScore += val;
                 }
             }
+            temp &= (temp - 1);
         }
-		
-		//Condition 3 : Diversité des contraintes 
-		int diverseLisereScore = 0;
-		long myPieces = board.getBlackPaladins() | board.getBlackUnicorn();
-        HashSet<Integer> lisereSet = new HashSet<>();
-        for(int i=0; i<36; i++) {
-            if((myPieces & (1L<<i))!=0) {
-                lisereSet.add(board.getLisereType(i));
-            }
-        }
-        diverseLisereScore = lisereSet.size();
 
-		return possibleMovesScore + dangerScore + diverseLisereScore; // à voir pour ajuster les poids de chaque condition 
-	};
-
+        return isDefensive ? (threatScore * 2) : threatScore;
+    }
 }
